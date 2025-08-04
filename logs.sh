946f5d5f449a3129a7484bbf0d37761a5fe3540d
K.Manohar <manohar.kunchala12@gmail.com>
2025-08-04 11:03:01 +0530

    project first file initiated

--- logs.sh
+++ logs.sh
@@ -10,3 +10,23 @@ then
 else 
     echo "you are running with access"
 fi 
+
+
+VALIDATE(){
+    if [ $? -eq 0]
+    then 
+        echo "installing $2 is success..."
+    else 
+        echo "installing $2 is failure"
+    fi
+}
+
+dnf list installed python3
+if [$? -ne 0]
+then
+    echo "python3 is not installed, it is going to be installed"
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo "python3 is laredy nstalled so nothing to do now, Thank you"
+fi
